dba4c003b6ce9416d1b57498f3d748d890968080
Ikal-Sama <joestarjosuke28@gmail.com>
2026-06-23 11:37:03 +0800

    Setup neon postgres and neon and Implement auth

---
+++ src/services/auth.service.js
@@ -0,0 +1,75 @@
+import logger from "#config/logger.js"
+import bcrypt from 'bcrypt'
+import { db } from '#config/database.js'
+import { eq } from 'drizzle-orm'
+import { users } from "../models/user.model.js"
+
+export const hashPassword = async(password) => {
+    try {
+        return await bcrypt.hash(password, 10);
+    } catch (e) {
+        logger.error('Failed to hash password', e);
+        throw new Error('Error hashing')
+    }
+}
+
+export const comparePassword = async(password, hash) => {
+    try {
+        return await bcrypt.compare(password, hash);
+    } catch (e) {
+        logger.error('Failed to compare password', e);
+        throw new Error('Error comparing password');
+    }
+}
+
+export const authenticateUser = async({ email, password }) => {
+    try {
+        const [existingUser] = await db.select({
+            id: users.id,
+            name: users.name,
+            email: users.email,
+            password: users.password,
+            role: users.role,
+            created_at: users.created_at,
+        })
+        .from(users)
+        .where(eq(users.email, email))
+        .limit(1);
+
+        if(!existingUser) throw new Error('User not found');
+
+        const isValid = await comparePassword(password, existingUser.password);
+
+        if(!isValid) throw new Error('Invalid credentials');
+
+        logger.info(`User ${existingUser.email} authenticated successfully`);
+
+        return existingUser;
+    } catch (e) {
+        logger.error(`Error authenticating user: ${e}`);
+        throw e;
+    }
+}
+
+export const createUser = async({ name, email, password, role = 'user'}) => {
+    try {
+        const existingUser = db.select().from(users).where(eq(users.email, email)).limit(1);
+        
+        if(existingUser.length > 0) throw new Error('User already exists')
+        
+        const password_hash = await hashPassword(password);
+        
+        const [newUser] = await db.insert(users)
+        .values({name, email, password: password_hash, role})
+        .returning({id: users.id, name: users.name, email: users.email, role: users.role, created_at: users.created_at})
+
+        logger.info(`User ${newUser.email} created successfully`)
+
+        return newUser;
+
+    } catch (e) {
+        logger.error(`Error creating the user: ${e}`)
+        throw e;
+    }
+}
+
