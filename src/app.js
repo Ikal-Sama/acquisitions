d4dc9855c0ef0305b6dc98e2fecba26f35f14a7d
Ikal-Sama <joestarjosuke28@gmail.com>
2026-06-24 10:22:31 +0800

    Implement CI/CD pipelines and GitHub actions

--- src/app.js
+++ src/app.js
@@ -7,42 +7,47 @@ import cookieParser from 'cookie-parser';
 import authRoutes from '#routes/auth.route.js';
 import usersRoutes from '#routes/users.route.js';
 import securityMiddleware from '#middleware/security.middleware.js';
-  
+
 const app = express();
 
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use(morgan('combined', {
-  stream: {
-    write: (message) => logger.info(message)
-  }
-}));
+app.use(
+  morgan('combined', {
+    stream: {
+      write: message => logger.info(message),
+    },
+  })
+);
 
-app.use(securityMiddleware)
+app.use(securityMiddleware);
 
 app.get('/', (req, res) => {
   logger.info('Hello from Acquisitions!');
-  
+
   res.status(200).send('Hello from Acquisitions!');
 });
 
 app.get('/health', (req, res) => {
-  res.status(200).json({ 
-    status: 'OK', 
+  res.status(200).json({
+    status: 'OK',
     timestamp: new Date().toISOString(),
-    uptime: process.uptime()
-  })
-})
+    uptime: process.uptime(),
+  });
+});
 
 app.get('/api', (req, res) => {
-  res.status(200).json({message: 'Acquisition API is running!'})
-})
+  res.status(200).json({ message: 'Acquisition API is running!' });
+});
 
 app.use('/api/auth', authRoutes);
-app.use('/api/users', usersRoutes)
+app.use('/api/users', usersRoutes);
+app.use((req, res) => {
+  res.status(404).json({ error: 'Route Not found!' });
+});
 
 export default app;
